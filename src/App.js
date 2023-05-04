423e68950664b682c9dd1babeb1d69f117c41b49
[your <sc2745@cornell.edu>
2023-05-03 21:35:40 -0400

    github init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// via "index.js" file to render "Pet Social" in <div id="root">
+
+// import Header from './Header';
+
+// contents below will be displayed in website
+function App() {
+    return "Pet Social"
+    // return <Header />;
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,8 @@
+// // import Menu from semantic
+// import { Menu } from 'semantic-ui-react'
+
+// function Header() {
+//     return <Menu />;
+// }
+
+// export default Header;
