613b989219aff00e63638427828cb010a496b331
sc2745 <112050607+sc2745@users.noreply.github.com>
2023-05-05 16:48:16 -0400

    Update PetContext.js

--- src/context/PetContext.js
+++ src/context/PetContext.js
@@ -1,10 +1,10 @@
-import { createContext, useEffect, useState } from "react";
-
+// import from firebase, react, and petService files
 import { getDoc } from "firebase/firestore";
-
+import { useEffect, createContext, useState } from "react";
 import { addPet, getAll, updatePet } from "../services/petService";
 
 
+// create the PetContext object with default values for each property.
 export const PetContext = createContext({
     pets: [],
     addNewPet: (pet) => { },
@@ -15,98 +15,123 @@ export const PetContext = createContext({
 })
 
 export const PetContextProvider = (props) => {
+
     const [pets, setPets] = useState([]);
 
+    // fetche all pets data from some external data source using getAll() function, then maps over the data to create an array of pets with id and other properties.
+    // then, updates the state of the component with the array of pets using setPets()
 
     useEffect(() => {
-        getAll().then((pets) => {
-            const currentPets = [];
+        try {
+            getAll().then((pets) => {
+                const currentPets = [];
 
-            pets.forEach((p) => {
-                const pet = {
-                    id: p.id,
-                    ...p.data()
-                }
-                currentPets.push(pet)
-            });
-            setPets(currentPets);
-        }).catch((error) => {
+                pets.forEach((p) => {
+                    const pet = {
+                        id: p.id,
+                        ...p.data()
+                    }
+                    currentPets.push(pet)
+                });
+                setPets(currentPets);
+            })
+        } catch(error) {
             console.log(error)
-        })
+        }
     }, [])
 
 
+    // create a enw profile for a pet
     const addNewPet = async (pet) => {
+        try {
 
-        const refPet = await addPet(pet);
-        const docSnap = await getDoc(refPet);
+            // create a pet
+            const refPet = await addPet(pet);
+            const docSnap = await getDoc(refPet);
+            const newPet = { id: docSnap.id, ...docSnap.data() };
+            setPets(prevPets => [...prevPets, newPet]);
 
-        setPets((prevState) =>
-            prevState.concat({
+        } catch (error) {
+          console.log(error);
+        }
+      };
 
-                id: docSnap.id,
-                name: docSnap.data().name,
-                bread: docSnap.data().bread,
-                imageUrl: docSnap.data().imageUrl,
-                description: docSnap.data().description,
-                ownerId: docSnap.data().ownerId,
-                likedBy: docSnap.data().likedBy,
-            })
-        );
-    };
-
-    const likePet = async (pet, userId) => {
-
-        let likedBy = pet.likedBy || [];
-        const updatedLikePet = { ...pet, likedBy: [...likedBy, userId] }
-
-        await updateCurrentPet(updatedLikePet);
-
-    }
-
-    const unLikePet = async (pet, userId) => {
-
-        const likedIndex = pet.likedBy.findIndex((id) => id === userId);
-
-        let likedBy = [...pet.likedBy];
-
-        likedBy = [
-            ...likedBy.slice(0, likedIndex),
-            ...likedBy.slice(likedIndex + 1),
-        ];
-
-        const updatedPet = { ...pet, likedBy: likedBy };
-
-        await updateCurrentPet(updatedPet);
-    }
-
-    const updateCurrentPet = async (pet) => {
-
-        const result = await updatePet(pet)
-        const index = pets.findIndex((p) => p.id === result.id)
-
-        const updatedPets = [...pets]
-        updatedPets[index] = { ...result, id: pet.id }
-
-
-        setPets(updatedPets)
-    }
-
+    // remove pet
     const removePet = (petId) => {
-        setPets((prevState) => prevState.filter((pet) => pet.id !== petId));
+
+        // find the pet
+        setPets((prevState) => prevState.filter(
+            (pet) => pet.id !== petId));
     };
 
+    // function to like a pet
+    const likePet = async (pet, userId) => {
+        try {
+
+            // find and like the pet
+            const likedBy = pet.likedBy ? [...pet.likedBy, userId] : [userId];
+
+            // update 
+            const updatedPet = { ...pet, likedBy };
+            await updateCurrentPet(updatedPet);
+
+        } catch (error) {
+            console.log(error);
+        }
+      };
+
+    // function to unlike the pet already liked before
+    const unLikePet = async (pet, userId) => {
+        try {
+
+            // find and remove the pet
+            const likedBy = pet.likedBy.filter(id => id !== userId);
+            
+            // update the pet
+            const updatedPet = { ...pet, likedBy };
+            await updateCurrentPet(updatedPet);
+
+        } catch (error) {
+            console.log(error);
+        }
+      };
+
+    // update status of pet
+    const updateCurrentPet = async (pet) => {
+        try {
+            const result = await updatePet(pet)
+            
+            //  not working
+            // const updatedPets = pets.map(p => p.id === result.id ? { id: result.id, ...result.data() } : p);
+            
+            // find pet
+            const index = pets.findIndex((p) => p.id === result.id)
+            const updatedPets = [...pets]
+
+            // update that pet
+            updatedPets[index] = { ...result, id: pet.id }   
+            setPets(updatedPets)
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    // return petContextValue
+    const petContextValue = {
+        pets,
+        addNewPet,
+        likePet,
+        unLikePet,
+        updateCurrentPet,
+        removePet
+      };
+
+    // return 
     return (
-        <PetContext.Provider
-            value={{
-                pets,
-                addNewPet,
-                updateCurrentPet,
-                removePet,
-                likePet,
-                unLikePet,
-            }}>
-            {props.children}
-        </PetContext.Provider>
+    <PetContext.Provider value={petContextValue}>
+        {props.children}
+    </PetContext.Provider>
     );
 } 
