f443bd92342d1a895b59d987a6b4839a059739a5
Katherine <67365874+kattt999@users.noreply.github.com>
2023-05-05 16:43:55 -0400

    Add files via upload

---
+++ src/context/PetContext.js
@@ -0,0 +1,112 @@
+import { createContext, useEffect, useState } from "react";
+
+import { getDoc } from "firebase/firestore";
+
+import { addPet, getAll, updatePet } from "../services/petService";
+
+
+export const PetContext = createContext({
+    pets: [],
+    addNewPet: (pet) => { },
+    updateCurrentPet: (pet) => { },
+    removePet: (petId) => { },
+    likePet: (pet, userId) => { },
+    unLikePet: (pet, userId) => { },
+})
+
+export const PetContextProvider = (props) => {
+    const [pets, setPets] = useState([]);
+
+
+    useEffect(() => {
+        getAll().then((pets) => {
+            const currentPets = [];
+
+            pets.forEach((p) => {
+                const pet = {
+                    id: p.id,
+                    ...p.data()
+                }
+                currentPets.push(pet)
+            });
+            setPets(currentPets);
+        }).catch((error) => {
+            console.log(error)
+        })
+    }, [])
+
+
+    const addNewPet = async (pet) => {
+
+        const refPet = await addPet(pet);
+        const docSnap = await getDoc(refPet);
+
+        setPets((prevState) =>
+            prevState.concat({
+
+                id: docSnap.id,
+                name: docSnap.data().name,
+                bread: docSnap.data().bread,
+                imageUrl: docSnap.data().imageUrl,
+                description: docSnap.data().description,
+                ownerId: docSnap.data().ownerId,
+                likedBy: docSnap.data().likedBy,
+            })
+        );
+    };
+
+    const likePet = async (pet, userId) => {
+
+        let likedBy = pet.likedBy || [];
+        const updatedLikePet = { ...pet, likedBy: [...likedBy, userId] }
+
+        await updateCurrentPet(updatedLikePet);
+
+    }
+
+    const unLikePet = async (pet, userId) => {
+
+        const likedIndex = pet.likedBy.findIndex((id) => id === userId);
+
+        let likedBy = [...pet.likedBy];
+
+        likedBy = [
+            ...likedBy.slice(0, likedIndex),
+            ...likedBy.slice(likedIndex + 1),
+        ];
+
+        const updatedPet = { ...pet, likedBy: likedBy };
+
+        await updateCurrentPet(updatedPet);
+    }
+
+    const updateCurrentPet = async (pet) => {
+
+        const result = await updatePet(pet)
+        const index = pets.findIndex((p) => p.id === result.id)
+
+        const updatedPets = [...pets]
+        updatedPets[index] = { ...result, id: pet.id }
+
+
+        setPets(updatedPets)
+    }
+
+    const removePet = (petId) => {
+        setPets((prevState) => prevState.filter((pet) => pet.id !== petId));
+    };
+
+    return (
+        <PetContext.Provider
+            value={{
+                pets,
+                addNewPet,
+                updateCurrentPet,
+                removePet,
+                likePet,
+                unLikePet,
+            }}>
+            {props.children}
+        </PetContext.Provider>
+    );
+} 
