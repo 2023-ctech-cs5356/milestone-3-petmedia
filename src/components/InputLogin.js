9ad1770c005f9fc11271f12eb0db9b4fcd227e28
sc2745 <112050607+sc2745@users.noreply.github.com>
2023-05-08 12:58:08 -0400

    Update InputLogin.js

--- src/components/InputLogin.js
+++ src/components/InputLogin.js
@@ -1,6 +1,10 @@
+// import react
 import { useState } from "react";
+
+// import css style
 import styled from "styled-components";
 
+// css style
 const FormInput = styled.div`
   display: flex;
   flex-direction: column;
@@ -10,42 +14,49 @@ const FormInput = styled.div`
 `;
 
 const LabelField = styled.label`
-  margin: 1.2rem;
-  font-size: 1.4rem;
+  margin: 1rem;
+  font-size: 1.5rem;
   font-weight: 500;
   color: #000;
 `;
 
 const InputField = styled.input`
-  height: 4rem;
+  height: 5rem;
   width: 30rem;
-  padding: 1.3rem 2rem;
+  padding: 1.2rem 2.1rem;
   border: 1px solid #555;
   border-radius: 7px;
 
   &[aria-invalid="true"] {
-    border: 1px solid red;
+    border: 1.3px solid red;
   }
 `;
 
 const ErrorMessage = styled.span`
-  font-size: 1.4rem;
-  padding: 0.5rem 2rem;
+  font-size: 1.5rem;
+  padding: 0.6rem 2.2rem;
   color: red;
   display: ${({ show }) => (show ? "block" : "none")};
 `;
 
+// represent a login input field. It takes in several props including label, errorMessage, onChange, id, and additional inputProps
+// use the useState hook to manage the focused state of the input field
 const InputLogin = ({ label, errorMessage, onChange, id, ...inputProps }) => {
+
+  // input element, a label, and an input field. 
   const [focused, setFocused] = useState(false);
 
+  // handler sets the focused state to true
   const handleFocus = () => {
     setFocused(true);
   };
 
+  // while the handleBlur event handler sets it to false.
   const handleBlur = () => {
     setFocused(false);
   };
 
+  // export inputLogin component
   return (
     <FormInput>
       <LabelField htmlFor={id}>{label}</LabelField>
@@ -63,3 +74,4 @@ const InputLogin = ({ label, errorMessage, onChange, id, ...inputProps }) => {
 };
 
 export default InputLogin;
+
