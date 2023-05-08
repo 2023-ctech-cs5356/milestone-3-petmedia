93b244cf4597f40bc2abcce18aa088ed63fd743d
sc2745 <112050607+sc2745@users.noreply.github.com>
2023-05-08 16:36:01 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
+// improt react and react-dom
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+
+// import authentication and pet context
 import { AuthContextProvider } from './context/AuthContext';
 import { PetContextProvider } from './context/PetContext';
+
+// import css style
+import './index.css';
+
+// import from app
+import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
