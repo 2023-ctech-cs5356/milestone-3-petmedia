bbae023a45548f8a6e251b7073716a5a9aa829dc
Ethan Huang <112031418+EthanCornell@users.noreply.github.com>
2023-05-07 22:12:13 -0400

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,130 +1,21 @@
-// import { useContext, useState } from "react";
-// import { useNavigate, Link } from "react-router-dom";
-
-// import { createUserWithEmailAndPassword } from "firebase/auth";
-// import { auth } from "../firebase";
-
-// import { AuthContext } from "../context/AuthContext";
-// import InputLogin from "../components/InputLogin";
-// import styles from "./Register.module.css";
-
-// const Register = () => {
-//     const inputs = [
-//         {
-//             id: 1,
-//             name: "email",
-//             type: "email",
-//             placeholder: "Email",
-//             errorMessage: "It should be a valid email adress",
-//             label: "Email",
-//             required: true,
-//         },
-//         {
-//             id: 2,
-//             name: "password",
-//             label: "Password",
-//             type: "password",
-//             placeholder: "Password",
-//             errorMessage:
-//                 "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
-//             pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
-//             required: true,
-//         },
-//         {
-//             id: 3,
-//             name: "repeatPassword",
-//             label: "Confirm Password",
-//             type: "password",
-//             placeholder: "Confirm Password",
-//             errorMessage: "Password doesn't match",
-//             //   pattern: "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$",
-//             required: true,
-//         },
-//     ];
-
-
-//     const [error, setError] = useState(false);
-
-//     const [values, setValues] = useState({
-//         email: "",
-//         password: "",
-//         repeatPassword: ""
-//     });
-
-//     const navigate = useNavigate();
-
-//     const { dispatch } = useContext(AuthContext);
-
-//     const onChange = (e) => {
-//         setValues({ ...values, [e.target.name]: e.target.value });
-//     };
-
-//     const handleLogin = (e) => {
-//         e.preventDefault();
-
-//         if (values.password !== values.repeatPassword) {
-//             setError(true)
-//             return;
-//         } else {
-//             setError(false)
-//         }
-
-//         createUserWithEmailAndPassword(auth, values.email, values.password)
-//             .then((userCredential) => {
-
-//                 const user = userCredential.user;
-//                 dispatch({ type: "REGISTER", payload: user });
-
-//                 navigate("/");
-
-//             })
-//             .catch((error) => {
-//                 setError(true);
-//             });
-//     };
-
-//     return (
-//         <div className={styles.register}>
-//             <form className={styles.formOne} onSubmit={handleLogin}>
-//                 {inputs.map((input) => (
-//                     <InputLogin
-//                         key={input.id}
-//                         {...input}
-//                         value={values[input.name]}
-//                         onChange={onChange}
-//                     />
-//                 ))}
-//                 <div className={styles["divBtn"]}>
-//                     <button className={styles["btnlogin"]} type="submit">
-//                         Register
-//                     </button>
-//                     <p className={styles["p-auth-info"]}>
-//                         Have already an account?{" "}
-//                         <Link className="link-auth" to="/login">
-//                             {" "}
-//                             Login
-//                         </Link>{" "}
-//                     </p>
-//                     {error && <p className={styles['error-register']}>passwords does not match</p>}
-//                 </div>
-//             </form>
-//         </div>
-//     );
-// };
-
-// export default Register;
-
+// import from react and react-router-dom
 import React, { useContext, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
+
+// import from firebase
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
+
+// import input login and authentication
+import InputLogin from "../components/InputLogin";
 import { AuthContext } from "../context/AuthContext";
 
-import InputLogin from "../components/InputLogin";
+// import css style
 import styles from "./Register.module.css";
 
 const Register = () => {
   const [error, setError] = useState(false);
+
   const [values, setValues] = useState({
     email: "",
     password: "",
@@ -134,6 +25,8 @@ const Register = () => {
   const navigate = useNavigate();
   const { dispatch } = useContext(AuthContext);
 
+  // handle situation when password doesn't match!
+  // https://stackoverflow.com/questions/1653425/a-za-z-a-za-z0-9-regular-expression
   const inputs = [
     {
       id: 1,
@@ -142,7 +35,7 @@ const Register = () => {
       label: "Email",
       placeholder: "Email",
       required: true,
-      errorMessage: "Please enter a valid email address",
+      errorMessage: "Enter a valid email address. Please re-enter again.",
     },
     {
       id: 2,
@@ -151,39 +44,40 @@ const Register = () => {
       label: "Password",
       placeholder: "Password",
       required: true,
-      errorMessage:
-        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
-      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      errorMessage: "",
     },
     {
       id: 3,
       name: "repeatPassword",
       type: "password",
-      label: "Confirm Password",
+      label: "Re-type Password",
       placeholder: "Confirm Password",
       required: true,
-      errorMessage: "Passwords do not match",
+      errorMessage: "Password doesn't not match. Please re-enter again.",
     },
   ];
 
   const handleInputChange = (event) => {
-    setValues({
-      ...values,
-      [event.target.name]: event.target.value,
-    });
+    const { name, value } = event.target;
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    if (values.password !== values.repeatPassword) {
+    const { password, repeatPassword, email } = values;
+
+    if (password !== repeatPassword) {
       setError(true);
       return;
-    } else {
-      setError(false);
     }
 
-    createUserWithEmailAndPassword(auth, values.email, values.password)
+    setError(false);
+
+    createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
         dispatch({ type: "REGISTER", payload: user });
@@ -196,6 +90,19 @@ const Register = () => {
 
   return (
     <div className={styles.register}>
+      <div className={styles.divBtn}>
+        <p className={styles["p-auth-info"]}>
+          Hello new user! Welcome to Petmedia.
+          <br></br>
+          Please create an account name with a valid Email and a password.
+          <br></br>
+          Please confirm password by re-typing again.
+          <br></br>
+          If you already have an account, you'r a returning user. Please check
+          below information to login.
+        </p>
+      </div>
+
       <form className={styles.formOne} onSubmit={handleSubmit}>
         {inputs.map((input) => (
           <InputLogin
@@ -203,32 +110,43 @@ const Register = () => {
             type={input.type}
             name={input.name}
             label={input.label}
-            placeholder={input.placeholder}
-            required={input.required}
-            errorMessage={input.errorMessage}
             pattern={input.pattern}
             value={values[input.name]}
             onChange={handleInputChange}
+            placeholder={input.placeholder}
+            required={input.required}
+            errorMessage={input.errorMessage}
           />
         ))}
+
         <div className={styles["divBtn"]}>
           <button className={styles["btnlogin"]} type="submit">
             Register
           </button>
           <p className={styles["p-auth-info"]}>
-            Already have an account?{" "}
+            Already have an account?
+            {/* <br></br>
+              If you don't have an account, you'r a new user. */}
+            <br></br>
+            Welcome back! Ready to share the photo and story about your pet?
+            Plase click the below Login to start!
+            <br></br>{" "}
             <Link className="link-auth" to="/login">
               {" "}
-              Log in
+              Login
             </Link>{" "}
           </p>
           {error && (
-            <p className={styles["error-register"]}>
-              {inputs[2].errorMessage}
-            </p>
+            <p className={styles["error-register"]}>{inputs[2].errorMessage}</p>
           )}
         </div>
       </form>
+
+      <p className="heading-tertiary contrast-color">
+        Join PetMedia today and become part of a vibrant and supportive
+        community that celebrates the love and companionship that pets bring to
+        our lives!
+      </p>
     </div>
   );
 };
