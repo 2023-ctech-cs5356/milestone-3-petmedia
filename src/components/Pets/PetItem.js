9978988f9db03efbcb03b80d276803064b0ef2ec
sc2745 <112050607+sc2745@users.noreply.github.com>
2023-05-08 16:52:35 -0400

    Update PetItem.js

--- src/components/Pets/PetItem.js
+++ src/components/Pets/PetItem.js
@@ -1,12 +1,13 @@
-
+// import react
 import { Link } from 'react-router-dom';
+
+// import css
 import styled from 'styled-components';
 
-const PetItemWrapper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 0;
-  margin-bottom: 9.6rem;
+const CardParagraph = styled.p`
+  font-size: 2rem;
+  line-height: 1.5;
+  padding: 0 1rem;
 `;
 
 const BreedsLeft = styled.div`
@@ -14,12 +15,11 @@ const BreedsLeft = styled.div`
   width: 50rem;
 `;
 
-const CardImage = styled.img`
-  object-fit: cover;
-  object-position: top;
-  display: block;
-  height: 100%;
-  width: 100%;
+const PetItemWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 0;
+  margin-bottom: 10rem;
 `;
 
 const BreedsRight = styled.div`
@@ -27,33 +27,35 @@ const BreedsRight = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  gap: 2rem;
+  gap: 3rem;
   background-color: ${props => (props.index % 2 !== 0 ? '#1a75bc' : '#fed201')};
 `;
 
+const CardImage = styled.img`
+  object-fit: cover;
+  object-position: top;
+  display: block;
+  height: 95%;
+  width: 95%;
+`;
+
 const CardTitle = styled.h4`
-  font-size: 3.5rem;
+  font-size: 3.6rem;
   padding: 1rem;
 `;
 
-const CardParagraph = styled.p`
-  font-size: 1.8rem;
-  line-height: 1.4;
-  padding: 0 1rem;
-`;
-
 const CardBtn = styled(Link)`
-  font-size: 1.8rem;
-  padding: 1rem 5rem;
+  font-size: 2rem;
+  padding: 1rem 4rem;
   margin-bottom: 1rem;
   cursor: pointer;
-  width: 15rem;
-  border-radius: 1rem;
+  width: 16rem;
+  border-radius: 1.5rem;
   font-weight: 600;
   border: none;
   color: #fff;
   background-color: #68b403;
-  transition: all 0.3s;
+  transition: all 0.25s;
   
   &:hover,
   &:active {
